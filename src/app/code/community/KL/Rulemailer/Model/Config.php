27bb9665fff320298a81090029ce84f1e0435f2f
Robert Lord <robert@codepeak.se>
2015-04-15 16:32:03 +0200

    Remove throwing exceptions, settings will be there when API key is saved

--- src/app/code/community/KL/Rulemailer/Model/Config.php
+++ src/app/code/community/KL/Rulemailer/Model/Config.php
@@ -14,20 +14,13 @@ class KL_Rulemailer_Model_Config extends KL_Rulemailer_Model_Abstract
      *
      * @param string $key The option name.
      * @param string $group The option group.
-     * @throws InvalidArgumentException if the given option does not exists.
+     *                      
      * @return string
      */
     public function get($key, $group = 'general')
     {
         $configKey = join(array(self::SECTION, $group, $key), '/');
 
-        if (!is_null(Mage::getStoreConfig($configKey))) {
-            return Mage::getStoreConfig($configKey);
-        } else {
-            throw new InvalidArgumentException(
-                'invalid config key: ' . $configKey
-            );
-        }
-
+        return Mage::getStoreConfig($configKey);
     }
 }
